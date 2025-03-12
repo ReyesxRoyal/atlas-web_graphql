d678419a38fa823b880449614167e2776649f2de
ReyesxRoyal <rasheed.r.white@gmail.com>
2025-03-11 23:36:05 -0500

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+Atlas-web graph ql
---
+++ server/app.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://abrielleperry22:m5MFaOfAuit571pL@filesmanager.x25kz.mongodb.net/?retryWrites=true&w=majority&appName=FilesManager', {
+});
+
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('Now listening for requests on port 4000');
+});
