debe105d459dcdd04b44ab0a637bd84cebea443d
ReyesxRoyal <rasheed.r.white@gmail.com>
2025-06-20 14:15:35 -0500

    mongoose

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
- const express = require('express');
+const express = require('express');
+const schema = require('./schema/schema');
 const {graphqlHTTP} = require('express-graphql');
-
 const app = express();
 
 app.use('/graphql',graphqlHTTP({
